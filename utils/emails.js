efcd8acf8625fd62aff2db63ce81ce301fb95394
Lean <gedeonleandro@gmail.com>
2025-06-14 20:26:21 -0300

    try

--- utils/emails.js
+++ utils/emails.js
@@ -2,11 +2,12 @@ const sgMail = require("@sendgrid/mail");
 const EMAIL_USER = "gerardfil.dev@gmail.com";
 const SUBJECT = "Invitacion a la plataforma de gerardfil";
 
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 async function sendEmail(to, html) {
   console.log("SENDGRID_API_KEY exists?", !!process.env.SENDGRID_API_KEY);
   console.log("SENDGRID_API_KEY:", process.env.SENDGRID_API_KEY?.slice?.(0, 5));
 
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
     to: to,
     from: EMAIL_USER,
